c25eddfc6f8092cb304abfda29a655048e92d07d
paedohunter <46535932+paedohunter@users.noreply.github.com>
2019-01-09 20:38:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $.getJSON("https://api.ipify.org/?format=json", function(e) {
-    $.getJSON("http://ip-api.com/json/" + e.ip, function(e) {
+    $.getJSON("https://extreme-ip-lookup.com/json/" + e.ip, function(e) {
     	$("#ip").html("IP: " + e.query + " (" + e.isp + ")")
-    	$("#location").html(e.city + ", " + e.regionName + ", " + e.country)
+    	$("#location").html(e.city + ", " + e.country)
 	})
 })
 
